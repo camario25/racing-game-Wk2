9a1a1415e710ca44ff5c4c2b6e3e5d122b99b241
camario25 <camario25@yahoo.com>
2018-02-02 10:08:05 -0800

    connected event listener to my document

--- app.js
+++ app.js
@@ -12,9 +12,25 @@ console.log('hello')
 // document.animationend() {
 //   window.alert( "Red Won!" );
 // }
+var car1 = document.getElementById("car1");
+var car2 = document.getElementById("car2");
 
+var carPosition = car1.style.left = 0 + "px";
+var carPosition = car2.style.left = 0 + "px";
+
+function move(car) {
+  var moveCar = car.style.left += 10;
+  return moveCar + "px";
+};
 document.addEventListener('keypress', function (event) {
   var keyCode = event.key;
-  console.log(keyCode)
-  alert(keyCode);
+  if (keyCode === 'z') {
+    console.log(keyCode);
+    move(car1)
+  }
+  else if (keyCode === 'm'){
+    console.log(keyCode);
+    move(car2);
+  }
 });
+
