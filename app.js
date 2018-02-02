f9ccc0e1f90317e3ce83c6a1b6e4b65bd0aebe2a
camario25 <camario25@yahoo.com>
2018-02-01 23:44:55 -0800

    initial commit for racing game

---
+++ app.js
@@ -0,0 +1,19 @@
+// document.onkeypress =
+// 
+// 
+// function move2em() {
+//   var move = document.getElementById("car1").style.margin += 10
+// }
+console.log('hello')
+document.addEventListener('keydown', function(event){
+    alert(event.key)
+}
+document.animationend() {
+  window.alert( "Red Won!" );
+}
+
+document.addEventListener('keypress', function (event) {
+  var keyCode = event.keycode;
+  console.log(keyCode)
+  alert(keyCode);
+});
