abd87c3528c5634017886dc984a3cfc8a5511da0
camario25 <camario25@yahoo.com>
2018-02-02 00:21:14 -0800

    connected js and was able to record a keypress event

--- app.js
+++ app.js
@@ -5,15 +5,16 @@
 //   var move = document.getElementById("car1").style.margin += 10
 // }
 console.log('hello')
-document.addEventListener('keydown', function(event){
-    alert(event.key)
-}
-document.animationend() {
-  window.alert( "Red Won!" );
-}
+
+// document.addEventListener('keydown', function(event){
+//     alert(event.key)
+// }
+// document.animationend() {
+//   window.alert( "Red Won!" );
+// }
 
 document.addEventListener('keypress', function (event) {
-  var keyCode = event.keycode;
+  var keyCode = event.key;
   console.log(keyCode)
   alert(keyCode);
 });
